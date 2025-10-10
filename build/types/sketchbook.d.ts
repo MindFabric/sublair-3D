5480575d02e96cd63d0bbaa751c9779c2a2b0d59
Pantheum <JamesMKemp@gmail.com>
2025-10-09 21:04:41 -0600

    001
    new plain map
    new terminal computer

--- build/types/sketchbook.d.ts
+++ build/types/sketchbook.d.ts
@@ -1,2 +1,8 @@
 import '../css/main.css';
 export { World } from './world/World';
+export { Character } from './characters/Character';
+export { Car } from './vehicles/Car';
+export { Airplane } from './vehicles/Airplane';
+export { Helicopter } from './vehicles/Helicopter';
+export { LoadingManager } from './core/LoadingManager';
+export { UIManager } from './core/UIManager';
--- src/ts/sketchbook.ts
+++ src/ts/sketchbook.ts
@@ -1,2 +1,21 @@
 import '../css/main.css';
+import * as THREE from 'three';
+import * as CANNON from 'cannon';
+import Swal from 'sweetalert2';
+import * as GUI from '../lib/utils/dat.gui';
+
+// Expose THREE, CANNON, Swal, and GUI globally
+if (typeof window !== 'undefined') {
+	(window as any).THREE = THREE;
+	(window as any).CANNON = CANNON;
+	(window as any).Swal = Swal;
+	(window as any).GUI = GUI;
+}
+
 export { World } from './world/World';
+export { Character } from './characters/Character';
+export { Car } from './vehicles/Car';
+export { Airplane } from './vehicles/Airplane';
+export { Helicopter } from './vehicles/Helicopter';
+export { LoadingManager } from './core/LoadingManager';
+export { UIManager } from './core/UIManager';
